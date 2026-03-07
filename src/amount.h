6dc62943717a33d68a3fb9acd4c156b241f31682
Rhett Creighton <2388024+RhettCreighton@users.noreply.github.com>
2026-03-07 10:44:51 +0700

    Convert amount and script_error from C++ to pure C23
    amount: Replace CFeeRate class with fee_rate struct and functions.
    script_error: Remove C++ namespace wrapper, rename set_success/set_error
    to avoid name collisions.

--- src/amount.h
+++ src/amount.h
@@ -1,64 +1,54 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+/* Copyright (c) 2009-2010 Satoshi Nakamoto
+ * Copyright (c) 2009-2014 The Bitcoin Core developers
+ * Copyright 2026 Rhett Creighton - Apache License 2.0
+ * Distributed under the MIT software license, see the accompanying
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php. */
 
 #ifndef BITCOIN_AMOUNT_H
 #define BITCOIN_AMOUNT_H
 
-#include "serialize.h"
-
-#include <stdlib.h>
-#include <string>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 typedef int64_t CAmount;
 
-static const CAmount COIN = 100000000;
-static const CAmount CENT = 1000000;
+#define COIN ((CAmount)100000000)
+#define CENT ((CAmount)1000000)
+#define MAX_MONEY (21000000 * COIN)
 
-extern const std::string CURRENCY_UNIT;
-
-/** No amount larger than this (in satoshi) is valid.
- *
- * Note that this constant is *not* the total money supply, which in Bitcoin
- * currently happens to be less than 21,000,000 BTC for various reasons, but
- * rather a sanity check. As this sanity check is used by consensus-critical
- * validation code, the exact value of the MAX_MONEY constant is consensus
- * critical; in unusual circumstances like a(nother) overflow bug that allowed
- * for the creation of coins out of thin air modification could lead to a fork.
- * */
-static const CAmount MAX_MONEY = 21000000 * COIN;
-inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
-
-/** Type-safe wrapper class to for fee rates
- * (how much to pay based on transaction size)
- */
-class CFeeRate
+static inline bool MoneyRange(CAmount nValue)
 {
-private:
-    CAmount nSatoshisPerK; // unit is satoshis-per-1,000-bytes
-public:
-    CFeeRate() : nSatoshisPerK(0) { }
-    explicit CFeeRate(const CAmount& _nSatoshisPerK): nSatoshisPerK(_nSatoshisPerK) { }
-    CFeeRate(const CAmount& nFeePaid, size_t nSize);
-    CFeeRate(const CFeeRate& other) { nSatoshisPerK = other.nSatoshisPerK; }
+    return nValue >= 0 && nValue <= MAX_MONEY;
+}
 
-    CAmount GetFee(size_t size) const; // unit returned is satoshis
-    CAmount GetFeePerK() const { return GetFee(1000); } // satoshis-per-1000-bytes
-
-    friend bool operator<(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK < b.nSatoshisPerK; }
-    friend bool operator>(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK > b.nSatoshisPerK; }
-    friend bool operator==(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK == b.nSatoshisPerK; }
-    friend bool operator<=(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK <= b.nSatoshisPerK; }
-    friend bool operator>=(const CFeeRate& a, const CFeeRate& b) { return a.nSatoshisPerK >= b.nSatoshisPerK; }
-    std::string ToString() const;
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITE(nSatoshisPerK);
-    }
+struct fee_rate {
+    CAmount satoshis_per_k;
 };
 
-#endif //  BITCOIN_AMOUNT_H
+static inline void fee_rate_init(struct fee_rate *r)
+{
+    r->satoshis_per_k = 0;
+}
+
+static inline void fee_rate_init_from_fee(struct fee_rate *r, CAmount fee_paid, size_t size)
+{
+    r->satoshis_per_k = size > 0 ? fee_paid * 1000 / (CAmount)size : 0;
+}
+
+static inline CAmount fee_rate_get_fee(const struct fee_rate *r, size_t size)
+{
+    CAmount fee = r->satoshis_per_k * (CAmount)size / 1000;
+    if (fee == 0 && r->satoshis_per_k > 0)
+        fee = r->satoshis_per_k;
+    return fee;
+}
+
+static inline CAmount fee_rate_get_fee_per_k(const struct fee_rate *r)
+{
+    return fee_rate_get_fee(r, 1000);
+}
+
+void fee_rate_to_string(const struct fee_rate *r, char *out, size_t out_size);
+
+#endif
--- src/script/script_error.h
+++ src/script/script_error.h
@@ -1,46 +1,38 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+/* Copyright (c) 2009-2010 Satoshi Nakamoto
+ * Copyright (c) 2009-2014 The Bitcoin Core developers
+ * Copyright 2026 Rhett Creighton - Apache License 2.0
+ * Distributed under the MIT software license, see the accompanying
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php. */
 
 #ifndef BITCOIN_SCRIPT_SCRIPT_ERROR_H
 #define BITCOIN_SCRIPT_SCRIPT_ERROR_H
 
-typedef enum ScriptError_t
-{
+#include <stdbool.h>
+
+typedef enum {
     SCRIPT_ERR_OK = 0,
     SCRIPT_ERR_UNKNOWN_ERROR,
     SCRIPT_ERR_EVAL_FALSE,
     SCRIPT_ERR_OP_RETURN,
-
-    /* Max sizes */
     SCRIPT_ERR_SCRIPT_SIZE,
     SCRIPT_ERR_PUSH_SIZE,
     SCRIPT_ERR_OP_COUNT,
     SCRIPT_ERR_STACK_SIZE,
     SCRIPT_ERR_SIG_COUNT,
     SCRIPT_ERR_PUBKEY_COUNT,
-
-    /* Failed verify operations */
     SCRIPT_ERR_VERIFY,
     SCRIPT_ERR_EQUALVERIFY,
     SCRIPT_ERR_CHECKMULTISIGVERIFY,
     SCRIPT_ERR_CHECKSIGVERIFY,
     SCRIPT_ERR_CHECKDATASIGVERIFY,
     SCRIPT_ERR_NUMEQUALVERIFY,
-
-    /* Logical/Format/Canonical errors */
     SCRIPT_ERR_BAD_OPCODE,
     SCRIPT_ERR_DISABLED_OPCODE,
     SCRIPT_ERR_INVALID_STACK_OPERATION,
     SCRIPT_ERR_INVALID_ALTSTACK_OPERATION,
     SCRIPT_ERR_UNBALANCED_CONDITIONAL,
-
-    /* OP_CHECKLOCKTIMEVERIFY */
     SCRIPT_ERR_NEGATIVE_LOCKTIME,
     SCRIPT_ERR_UNSATISFIED_LOCKTIME,
-
-    /* BIP62 */
     SCRIPT_ERR_SIG_HASHTYPE,
     SCRIPT_ERR_SIG_DER,
     SCRIPT_ERR_MINIMALDATA,
@@ -50,33 +42,24 @@ typedef enum ScriptError_t
     SCRIPT_ERR_PUBKEYTYPE,
     SCRIPT_ERR_CLEANSTACK,
     SCRIPT_ERR_SIG_NULLFAIL,
-
-    /* softfork safeness */
     SCRIPT_ERR_DISCOURAGE_UPGRADABLE_NOPS,
-
     SCRIPT_ERR_ERROR_COUNT
 } ScriptError;
 
 #define SCRIPT_ERR_LAST SCRIPT_ERR_ERROR_COUNT
 
-const char* ScriptErrorString(const ScriptError error);
+const char *ScriptErrorString(ScriptError error);
 
-namespace {
-
-inline bool set_success(ScriptError* ret)
+static inline bool set_script_success(ScriptError *ret)
 {
-    if (ret)
-        *ret = SCRIPT_ERR_OK;
+    if (ret) *ret = SCRIPT_ERR_OK;
     return true;
 }
 
-inline bool set_error(ScriptError* ret, const ScriptError serror)
+static inline bool set_script_error(ScriptError *ret, ScriptError serror)
 {
-    if (ret)
-        *ret = serror;
+    if (ret) *ret = serror;
     return false;
 }
 
-} // namespace
-
-#endif // BITCOIN_SCRIPT_SCRIPT_ERROR_H
+#endif
